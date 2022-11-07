20ee259c944b305afd9d71dbccf4d5bd70c1dc79
kikinacademy <kikinacademy@gmail.com>
2022-11-07 14:03:12 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,14 +1,26 @@
 USE TrackEmUP;
+# SELECTS ALL COUNTRIES
+SELECT *
+FROM TrackEmUP.country
+ORDER BY CountryID desc;
+
+# SELECTS PLAYER NAME, NATIONALITY, POSITION, FOOT & TEAM
+SELECT PlayerName, PlayerNationality, PlayerPosition, PlayerFoot, PlayerTeam
+FROM TrackEmUP.player
+GROUP BY PlayerName, PlayerPosition
+ORDER BY PlayerPosition;
 
 # SELECTS COUNTRIES WITH A LEAGUE
-SELECT CountryID, Country, LeagueName, LeagueID FROM TrackEmUP.league
-JOIN TrackEmUP.country
-ON TrackEmUP.league.Country = TrackEmUP.country.CountryName;
+SELECT CountryID, Country, LeagueName, LeagueID
+FROM TrackEmUP.league
+         JOIN TrackEmUP.country
+              ON TrackEmUP.league.Country = TrackEmUP.country.CountryName;
 
 # COUNT HOW MANY TEAMS ARE IN EACH LEAGUE
-SELECT LeagueName, COUNT(TeamName) AS TeamCount FROM TrackEmUP.team
-JOIN TrackEmUP.league
-ON TrackEmUP.team.TeamLeague = TrackEmUP.league.LeagueName
+SELECT LeagueName, COUNT(TeamName) AS TeamCount
+FROM TrackEmUP.team
+         JOIN TrackEmUP.league
+              ON TrackEmUP.team.TeamLeague = TrackEmUP.league.LeagueName
 GROUP BY LeagueName
 ORDER BY TeamCount DESC;
 
