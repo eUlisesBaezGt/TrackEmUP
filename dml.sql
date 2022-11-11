4b1eac7e93b860d0f6c14feded1330c1dd4bf0a2
kikinacademy <kikinacademy@gmail.com>
2022-11-11 15:17:34 -0600

    More functions for teams, positions, players

--- dml.sql
+++ dml.sql
@@ -31,7 +31,11 @@ VALUES ('Pep Guardiola', 'Spain'),
        ('Zinedine Zidane', 'France'),
        ('Jurgen Klopp', 'Germany'),
        ('Carlo Ancelotti', 'Italy'),
-       ('Alex Ferguson', 'United Kingdom');
+       ('Alex Ferguson', 'United Kingdom'),
+       ('José Mourinho', 'Portugal'),
+       ('Lionel Scaloni', 'Argentina'),
+       ('Tite', 'Brazil');
+
 
 
 ALTER TABLE `TrackEmUP`.`Stadium`
@@ -41,7 +45,8 @@ VALUES ('Camp Nou', 99354, 'Barcelona', 'Spain'),
        ('Parc des Princes', 47938, 'Paris', 'France'),
        ('Allianz Arena', 75000, 'Munich', 'Germany'),
        ('San Siro', 80000, 'Milan', 'Italy'),
-       ('Old Trafford', 76212, 'Manchester', 'United Kingdom');
+       ('Old Trafford', 76212, 'Manchester', 'United Kingdom'),
+       ('Etihad Stadium', 53400, 'Manchester', 'United Kingdom');
 
 
 ALTER TABLE `TrackEmUP`.`Team`
@@ -56,8 +61,10 @@ VALUES ('Real Madrid', 'Madrid', 'Madrid', 'Spain', 'La Liga', 'Pep Guardiola', 
 
 INSERT INTO `TrackEmUP`.`Team`(TeamName, TeamCity, TeamState, TeamCountry, TeamLeague, TeamManager, TeamStadium)
 VALUES ('Barcelona', 'Barcelona', 'Catalonia', 'Spain', 'La Liga', 'Pep Guardiola', 'Camp Nou'),
-       ('Manchester City', 'Manchester', 'England', 'United Kingdom', 'Premier League', 'Alex Ferguson',
-        'Old Trafford');
+       ('Manchester United', 'Manchester', 'England', 'United Kingdom', 'Premier League', 'Alex Ferguson',
+        'Old Trafford'),
+       ('Manchester City', 'Manchester', 'England', 'United Kingdom', 'Premier League', 'José Mourinho',
+        'Etihad Stadium');
 
 
 ALTER TABLE `TrackEmUP`.`Player`
@@ -68,7 +75,38 @@ VALUES ('Sergio Ramos', 'Spain', 'CB', 35, 184, 82, 'R', 'Real Madrid'),
        ('Kylian Mbappe', 'France', 'ST', 23, 178, 73, 'R', 'Paris Saint-Germain'),
        ('Leonardo Bonucci', 'Italy', 'CB', 34, 187, 85, 'R', 'Juventus'),
        ('Manuel Neuer ', 'Germany', 'GK', 35, 193, 92, 'R', 'Bayern Munich'),
-       ('Harry Kane', 'United Kingdom', 'ST', 27, 188, 89, 'R', 'Tottenham Hotspur');
+       ('Harry Kane', 'United Kingdom', 'ST', 27, 188, 89, 'R', 'Tottenham Hotspur'),
+       ('Lionel Messi', 'Argentina', 'ST', 34, 170, 72, 'R', 'Barcelona'),
+       ('Cristiano Ronaldo', 'Portugal', 'ST', 36, 187, 83, 'R', 'Juventus'),
+       ('Paul Pogba', 'France', 'CM', 28, 191, 84, 'R', 'Manchester United'),
+       ('Neymar', 'Brazil', 'ST', 29, 175, 68, 'R', 'Paris Saint-Germain'),
+       ('Luka Modric', 'Croatia', 'CM', 35, 172, 66, 'R', 'Real Madrid'),
+       ('Sergio Aguero', 'Argentina', 'ST', 33, 173, 70, 'R', 'Manchester City'),
+       ('Antoine Griezmann', 'France', 'ST', 30, 176, 73, 'R', 'Barcelona'),
+       ('Raheem Sterling', 'United Kingdom', 'RW', 27, 170, 70, 'R', 'Manchester City'),
+       ('Toni Kroos', 'Germany', 'CM', 31, 183, 76, 'R', 'Real Madrid'),
+       ('Karim Benzema', 'France', 'ST', 33, 185, 81, 'R', 'Real Madrid'),
+       ('Gareth Bale', 'United Kingdom ', 'RW', 31, 183, 79, 'R', 'Real Madrid'),
+       ('Thiago Silva', 'Brazil', 'CB', 36, 183, 79, 'R', 'Paris Saint-Germain'),
+       ('Marcelo', 'Brazil', 'LB', 34, 174, 66, 'R', 'Real Madrid'),
+       ('Sergio Busquets', 'Spain', 'CM', 33, 189, 76, 'R', 'Barcelona'),
+       ('Gerard Pique', 'Spain', 'CB', 34, 194, 85, 'R', 'Barcelona'),
+       ('Luis Nani', 'Portugal', 'RW', 35, 175, 69, 'R', 'Manchester United'),
+       ('David De Gea', 'Spain', 'GK', 30, 192, 82, 'R', 'Manchester United'),
+       ('Paulo Dybala', 'Argentina', 'ST', 27, 177, 75, 'R', 'Juventus'),
+       ('Andrea Pirlo', 'Italy', 'CM', 41, 180, 75, 'R', 'Juventus'),
+       ('Gianluigi Buffon', 'Italy', 'GK', 43, 191, 92, 'R', 'Juventus'),
+       ('Gonzalo Higuain', 'Argentina', 'ST', 33, 185, 81, 'R', 'Juventus'),
+       ('Mario Mandzukic', 'Croatia', 'ST', 34, 191, 88, 'R', 'Juventus'),
+       ('Leon Goretzka', 'Germany', 'CM', 26, 188, 84, 'R', 'Bayern Munich'),
+       ('Thomas Muller', 'Germany', 'ST', 32, 183, 76, 'R', 'Bayern Munich'),
+       ('Joshua Kimmich', 'Germany', 'CM', 26, 183, 76, 'R', 'Bayern Munich'),
+       ('Mats Hummels', 'Germany', 'CB', 32, 191, 92, 'R', 'Bayern Munich'),
+       ('Serge Gnabry', 'Germany', 'RW', 25, 180, 75, 'R', 'Bayern Munich'),
+       ('Dele Alli', 'United Kingdom', 'CM', 25, 178, 74, 'R', 'Tottenham Hotspur'),
+       ('Eric Dier', 'United Kingdom', 'CM', 27, 183, 76, 'R', 'Tottenham Hotspur'),
+       ('Hugo Lloris', 'France', 'GK', 34, 188, 84, 'R', 'Tottenham Hotspur'),
+       ('Kyle Walker', 'United Kingdom', 'RB', 30, 183, 76, 'R', 'Tottenham Hotspur');
 
 
 INSERT INTO `TrackEmUP`.`Referee`(RefereeName, RefereeAge, RefereeNationality, RefereeCategory)
@@ -76,7 +114,10 @@ VALUES ('Pierluigi Collina', 60, 'Italy', 'Legend'),
        ('Howard Webb', 50, 'United Kingdom', 'Elite'),
        ('Mark Clattenburg', 50, 'United Kingdom', 'Elite'),
        ('Massimiliano Irrati', 50, 'Italy', 'Legend'),
-       ('Michael Oliver', 50, 'United Kingdom', 'Elite');
+       ('Michael Oliver', 50, 'United Kingdom', 'Elite'),
+       ('Nikola Rizzoli', 50, 'Italy', 'Legend'),
+       ('Carlos Velasco Carballo', 50, 'Spain', 'Medium');
+
 
 
 ALTER TABLE `TrackEmUP`.`Match`
@@ -96,8 +137,11 @@ INSERT INTO `TrackEmUP`.`Events`(EventName)
 VALUES ('Goal'),
        ('Yellow Card'),
        ('Red Card'),
-       ('Substitution'),
-       ('Penalty');
+       ('Foul'),
+       ('Offside'),
+       ('Penalty'),
+       ('Injury'),
+       ('Own Goal');
 
 
 ALTER TABLE `TrackEmUP`.`MatchEvent`
--- functions.sql
+++ functions.sql
@@ -41,7 +41,8 @@ BEGIN
     SELECT * FROM TrackEmUP.Match WHERE MatchDate BETWEEN sDate AND eDate;
 END;
 
-CALL TrackEmUP.`TrackEmUP.SearchMatchesByDate`('2022-01-01', '2022-06-30'); # RETURNS 2 ROWS AFTER SECOND INSERT
+CALL TrackEmUP.`TrackEmUP.SearchMatchesByDate`('2022-01-01', '2022-06-30');
+# RETURNS 2 ROWS AFTER SECOND INSERT
 
 
 # CREATE A FUNCTION THAT SHOWS ALL PLAYERS WITH RED CARDS
@@ -50,38 +51,41 @@ DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchPlayersWithRedCards`;
 CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchPlayersWithRedCards`()
 BEGIN
     SELECT MatchEventID, MatchID, matchEvent.PlayerID, PlayerName, EventTime, Comments
-        FROM TrackEmUP.MatchEvent
-        JOIN TrackEmUP.Player ON matchEvent.PlayerID = Player.PlayerID
-        WHERE EventType = 3;
+    FROM TrackEmUP.MatchEvent
+             JOIN TrackEmUP.Player ON matchEvent.PlayerID = Player.PlayerID
+    WHERE EventType = 3;
 END;
 
-CALL TrackEmUP.`TrackEmUP.SearchPlayersWithRedCards`(); # RETURNS 1 ROW
+CALL TrackEmUP.`TrackEmUP.SearchPlayersWithRedCards`();
+# RETURNS 1 ROW
 
 # CREATE A FUNCTION THAT SHOWS ALL PLAYERS WITH YELLOW CARDS
 DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchPlayersWithYellowCards`;
 
 CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchPlayersWithYellowCards`()
 BEGIN
-SELECT MatchEventID, MatchID, matchEvent.PlayerID, PlayerName, EventTime, Comments
-        FROM TrackEmUP.MatchEvent
-        JOIN TrackEmUP.Player ON matchEvent.PlayerID = Player.PlayerID
-        WHERE EventType = 2;
+    SELECT MatchEventID, MatchID, matchEvent.PlayerID, PlayerName, EventTime, Comments
+    FROM TrackEmUP.MatchEvent
+             JOIN TrackEmUP.Player ON matchEvent.PlayerID = Player.PlayerID
+    WHERE EventType = 2;
 END;
 
-CALL TrackEmUP.`TrackEmUP.SearchPlayersWithYellowCards`(); # RETURNS 1 ROW
+CALL TrackEmUP.`TrackEmUP.SearchPlayersWithYellowCards`();
+# RETURNS 1 ROW
 
 # CREATE A FUNCTION THAT SHOWS ALL PLAYERS WITH GOALS
 DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchPlayersWithGoals`;
 
 CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchPlayersWithGoals`()
 BEGIN
-SELECT MatchEventID, MatchID, matchEvent.PlayerID, PlayerName, EventTime, Comments
-        FROM TrackEmUP.MatchEvent
-        JOIN TrackEmUP.Player ON matchEvent.PlayerID = Player.PlayerID
-        WHERE EventType = 1;
+    SELECT MatchEventID, MatchID, matchEvent.PlayerID, PlayerName, EventTime, Comments
+    FROM TrackEmUP.MatchEvent
+             JOIN TrackEmUP.Player ON matchEvent.PlayerID = Player.PlayerID
+    WHERE EventType = 1;
 END;
 
-CALL TrackEmUP.`TrackEmUP.SearchPlayersWithGoals`(); # RETURNS 1 ROW
+CALL TrackEmUP.`TrackEmUP.SearchPlayersWithGoals`();
+# RETURNS 1 ROW
 
 # CREATE A FUNCTION THAT SHOWS ALL THE GAMES OF A TEAM
 DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchMatchesByTeam`;
@@ -91,15 +95,122 @@ BEGIN
     SELECT * FROM TrackEmUP.Match WHERE MatchHomeTeam = teamName OR MatchAwayTeam = teamName;
 END;
 
-CALL TrackEmUP.`TrackEmUP.SearchMatchesByTeam`('Barcelona'); # RETURNS 1 ROW
+CALL TrackEmUP.`TrackEmUP.SearchMatchesByTeam`('Barcelona');
+# RETURNS 1 ROW
 
 # CREATE A FUNCTION THAT SHOWS ALL THE GAMES WITH A SPECIFIC REFEREE
 DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchMatchesByReferee`;
 
 CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchMatchesByReferee`(refID INT)
 BEGIN
-    SELECT MatchID, MatchDate, MatchHomeTeam, ScoreHomeTeam, MatchAwayTeam, ScoreAwayTeam, MatchLeague, MatchStadium
-        FROM TrackEmUP.Match WHERE RefereeID = refID;
+    SELECT MatchID,
+           MatchDate,
+           MatchHomeTeam,
+           ScoreHomeTeam,
+           MatchAwayTeam,
+           ScoreAwayTeam,
+           MatchLeague,
+           MatchStadium
+    FROM TrackEmUP.Match
+    WHERE RefereeID = refID;
 END;
 
-CALL TrackEmUP.`TrackEmUP.SearchMatchesByReferee`(1); # RETURNS 1 ROW
+CALL TrackEmUP.`TrackEmUP.SearchMatchesByReferee`(1);
+# RETURNS 1 ROW
+
+# CREATE A FUNCTION THAT SHOWS ALL THE GAMES IN A STADIUM
+DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchMatchesByStadium`;
+
+CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchMatchesByStadium`(stadiumName VARCHAR(250))
+BEGIN
+    SELECT MatchID,
+           MatchDate,
+           MatchHomeTeam,
+           ScoreHomeTeam,
+           MatchAwayTeam,
+           ScoreAwayTeam,
+           MatchLeague,
+           MatchStadium
+    FROM TrackEmUP.Match
+    WHERE MatchStadium = stadiumName;
+END;
+
+CALL TrackEmUP.`TrackEmUP.SearchMatchesByStadium`('Camp Nou');
+
+# SHOW ALL MATCH EVENTS WHERE A PLAYER WAS INVOLVED
+DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchMatchEventsByPlayer`;
+
+CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchMatchEventsByPlayer`(ID INT)
+BEGIN
+    SELECT matchevent.MatchEventID, matchevent.MatchID, Events.EventName, matchevent.EventTime, matchevent.Comments
+    FROM TrackEmUP.MatchEvent
+             JOIN TrackEmUP.Events ON matchevent.EventType = Events.EventID
+    WHERE matchevent.PlayerID = ID;
+END;
+
+CALL TrackEmUP.`TrackEmUP.SearchMatchEventsByPlayer`(1);
+# RETURNS 2 ROWS
+
+
+# SHOW ALL PLAYERS OF A TEAM
+DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchPlayersByTeam`;
+
+CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchPlayersByTeam`(teamName VARCHAR(250))
+BEGIN
+    SELECT player.playerid,
+           playername,
+           playernationality,
+           playerposition,
+           playerage,
+           playerheight,
+           playerweight,
+           playerfoot
+    FROM TrackEmUP.Player
+    WHERE Player.PlayerTeam = teamName;
+END;
+
+CALL TrackEmUP.`TrackEmUP.SearchPlayersByTeam`('Barcelona');
+# RETURNS 4 ROWS
+
+# SHOW ALL PLAYERS OF A TEAM WITH A SPECIFIC POSITION
+DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchPlayersByTeamAndPosition`;
+
+CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchPlayersByTeamAndPosition`(teamName VARCHAR(250), position VARCHAR(3))
+BEGIN
+    SELECT player.playerid,
+           playername,
+           playernationality,
+           playerposition,
+           playerage,
+           playerheight,
+           playerweight,
+           playerfoot
+    FROM TrackEmUP.Player
+    WHERE Player.PlayerTeam = teamName
+      AND Player.PlayerPosition = position;
+END;
+
+CALL TrackEmUP.`TrackEmUP.SearchPlayersByTeamAndPosition`('Barcelona', 'ST');
+# RETURNS 2 ROWS
+
+# SHOW ALL PLAYERS OF A TEAM WITH A SPECIFIC POSITION
+DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchPlayersByPosition`;
+
+CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchPlayersByPosition`(position VARCHAR(3))
+BEGIN
+    SELECT * FROM TrackEmUP.Player WHERE Player.PlayerPosition = position;
+END;
+
+CALL TrackEmUP.`TrackEmUP.SearchPlayersByPosition`('ST');
+# RETURNS 12 ROWS
+
+
+# SHOW ALL PLAYERS OF A NATIONALITY
+DROP PROCEDURE IF EXISTS TrackEmUP.`TrackEmUP.SearchPlayersByNationality`;
+
+CREATE PROCEDURE TrackEmUP.`TrackEmUP.SearchPlayersByCountry`(country VARCHAR(250))
+BEGIN
+    SELECT * FROM TrackEmUP.Player WHERE Player.PlayerNationality = country;
+END;
+
+CALL TrackEmUP.`TrackEmUP.SearchPlayersByCountry`('Spain'); # RETURNS 4 ROWS
